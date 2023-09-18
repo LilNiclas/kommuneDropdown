9e55734a5b7d3efe1115c9ab72a05a81e8aabf8a
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-09-18 18:07:26 +0200

    Able to search for a Kommune and get a reference link to it

--- js/kommuneDropdown.js
+++ js/kommuneDropdown.js
@@ -1,11 +1,23 @@
 console.log("We're in kommuneDropdown.js bois!")
 
+//Variables
 const urlKommune = "https://api.dataforsyningen.dk/kommuner"
+const ddKommuner = document.getElementById("ddKommuner")
+const pbFetchKommuner = document.getElementById("pbFetchKommuner")
+const searchKommuneInp = document.getElementById("searchKommune")
+const aTags = document.getElementById("a-tags")
+const kommuneDataMap = new Map
 
 //Fetch kommuner from API
 function fetchKommuner(any) {
     console.log("Inside fetchKommuner: " + any)
     return fetch(any).then(response => response.json())
+}
+
+//Fetches and fills dropdown when activated
+async function actionFetch() {
+    const kommuner = await fetchKommuner(urlKommune)
+    fillDropdown(kommuner)
 }
 
 //Fills dropdown with fecthed kommuner
@@ -14,18 +26,35 @@ function fillDropdown(kommuner) {
     kommuner.forEach(kommune => {
         const option = document.createElement("option")
         option.textContent = kommune.navn
-        option.value = kommune.kode
+        option.value = kommune.navn
         ddKommuner.appendChild(option)
+
+        //Store kommuner in map, with name as key
+        kommuneDataMap.set(kommune.navn, {
+            name: kommune.navn,
+            code: kommune.kode,
+            href: `${urlKommune}/${kommune.kode}`
+        })
     })
 }
 
-//Fetches and fills dropdown when activated
-async function actionFetch() {
-    const kommuner = await fetchKommuner(urlKommune)
-    console.log(kommuner)
-    fillDropdown(kommuner)
+//Creates an <a> tag for a Kommune
+function createATag(kommune) {
+    if (!kommune.used) {
+        const aTag = document.createElement("a")
+        aTag.textContent = kommune.name
+        aTag.href = kommune.href
+        aTags.appendChild(aTag)
+    }
 }
 
-const ddKommuner = document.getElementById("ddKommuner")
-const pbFetchKommuner = document.getElementById("pbFetchKommuner")
+//Detect input changes, only shows and creates aTag if the input matches a kommune
+searchKommuneInp.addEventListener("input", function () {
+    const inputText = this.value.trim();
+    aTags.innerHTML = ""
+    if (inputText && kommuneDataMap.has(inputText)) {
+        createATag(kommuneDataMap.get(inputText))
+    }
+})
+
 pbFetchKommuner.addEventListener('click', actionFetch)
